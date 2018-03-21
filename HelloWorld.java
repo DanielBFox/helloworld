877744e22301da0088a7cff2b288ceb5245bd18c
DanielBFox <daniel.bellido@outlook.be>
2018-03-21 10:54:00 +0000

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("HelloWorld 2.0 from Java Application running in Docker. ");
+        System.out.println("HelloWorld 3.0 from Java Application running in Docker. ");
     }
 }
